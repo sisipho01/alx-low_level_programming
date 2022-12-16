b82e6ec24d904eeaff2ff5ef9f91692789320976
sisipho01 <magqwashesisipho@gmail.com>
2022-12-16 09:54:55 -0800

    print_diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,24 +7,23 @@
 
 void print_diagonal(int n)
 {
-	if (n <= 0)
+	int i = 0;
+	int j;
+
+	if (n > 0)
 	{
-		_putchar('\n');
+		for (; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
 	}
 	else
 	{
-		int i, j;
-
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0; j < n; j++)
-			{
-				 if (j == i)
-					 _putchar('\\');
-				  else if (j < i)
-					   _putchar(' ');
-			}
-			_putchar('\n');
-		}
+		_putchar('\n');
 	}
 }
